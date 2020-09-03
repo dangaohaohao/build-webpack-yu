09138e6a87770ab6aebe3870b7ad49d9a6ed2c59
yuzhenliu <yuzhenliu163@163.com>
2020-09-03 11:47:09 +0800

    feat: 修改测试用例

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/liuyuzhen/Documents/liuyuzhen workspace/React Webpack/test-webpack-01/build-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/liuyuzhen/Documents/liuyuzhen workspace/React Webpack/test-webpack-01/build-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
